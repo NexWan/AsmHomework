d2cf38c06a9937587ba93400e9240521ef256596
LEONARDO CONTRERAS MARTINEZ <L21051403@saltillo.tecnm.mx>
2023-11-28 19:23:54 -0600

    erm

--- fibo.asm
+++ fibo.asm
@@ -1,4 +1,4 @@
-.model small
+                                               .model small
 .stack 100h
 .data
     buffer db 10
@@ -8,67 +8,86 @@
     cant db 0
     n_l DB 10D, 13D, "$"
     result DB ?
+    cont DB 0
     text1 DB "Teclee un numero (0-9): ", "$"
     divv DB ?
     remainder DB ?
 .code
-; Imprime un caracter en formato ascii
     printF macro num
         mov ah, 2
         mov dl, num
         int 21h
     ENDM
-    ; Metodo para input
+    
     askForInput macro num
         mov ah,1
         int 21h
+        add al, 1   
         sub al, '0'
         dec al
         mov [num], al
     ENDM
-    ; Metodo para imprimir un caracter en forma de numero
+
     print macro num
         mov ah,2
         mov dl, num
         add dl, '0'
         int 21h
     ENDM
-    ; Metodo para imprimir un string
+    
     printS macro string
         lea dx, string
         mov ah, 9
         int 21h
     ENDM
-    ;Metodo para leer numeros
+
     getDigits MACRO num
+    local CYCLE1
+    mov cont, 0
+    xor ax,ax
         CYCLE1:
             mov al, num
             AAM
             mov remainder, al
             mov divv, ah
             mov num, al
-            print remainder
+            push word ptr num
             mov ah, divv
+            mov num, ah
+            inc cont
             cmp ah, 0
         JNE CYCLE1
+
+        CYCLE2: 
+            xor ax, ax
+            pop dx
+            add dl, '0'
+            mov ah, 2
+            int 21h
+            xor dl, dl
+            dec cont
+            cmp cont, 0
+            JNE CYCLE2
     ENDM
     START:
         mov ax, @data
         mov ds, ax
         xor cx, cx
-        printS text1 ; Se imprime el texto de input
-        askForInput cant ; Se procesa el input
-        printS n_l ; salto de linea
-        mov cl, cant ; Se mueve a cl la cantidad, si borras el label de input reemplazalo por un 7 que es hasta donde lee el programa por alguna razon
-        fibo: ; Logica para fibonacci
-            getDigits z
+        printS text1
+        askForInput cant
+        printS n_l
+        mov cx, 9
+        print x
+        printS n_l
+        mov bh, y
+        fibo:
+            mov result, bh
+            getDigits result
             mov bl, x
-            add z, bl
-            mov bl, y
-            add z, bl
             mov bh, y
             mov x, bh
-            mov y, bl
+            add bh, bl
+            mov y, bh
             printS n_l
         LOOP fibo
         mov ax, 4c00h
